b94692a50d642e7c1f0f5fe82974d0ebcb6720e2
Nicola Roberts <nr3@sanger.ac.uk>
2015-08-26 14:47:09 +0100

    message on package attach

---
+++ R/zzz.R
@@ -0,0 +1,3 @@
+.onAttach <- function(libname, pkgname){
+  packageStartupMessage("Run citation('hdp') for citation instructions, and file.show(system.file('LICENSE', package='hdp')), for license details.")
+}
