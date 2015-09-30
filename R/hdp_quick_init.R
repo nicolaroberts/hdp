3f9ff07c6dc50a79d5214fd9814db7c8ee29bb4c
Nicola Roberts <nr3@sanger.ac.uk>
2015-09-30 17:18:57 +0100

    minor edits

--- R/hdp_quick_init.R
+++ R/hdp_quick_init.R
@@ -1,23 +1,23 @@
 #' Initialise a simple, default HDP structure
-#' 
-#' Initialise a hdpState object with a basic default structure of one top parent DP node 
+#'
+#' Initialise a hdpState object with a basic default structure of one top parent DP node
 #' with no associated data, and one child DP node per row of data. Every DP node
-#' shares the same concentration parameter, and will automatically be 'activated' 
+#' shares the same concentration parameter, and will automatically be 'activated'
 #' (made available for posterior samplig). The base distribution is a uniform Dirichlet
 #' with psuedocount of '1' in each data category. Can immediately run \code{\link{hdp_posterior}}
-#' to collect posterior samples. 
-#' 
-#' To define a custom HDP structure, see \code{\link{hdp_init}}, \code{\link{hdp_adddp}}, 
-#' and \code{\link{hdp_addconparam}}. 
-#' 
+#' to collect posterior samples.
+#'
+#' To define a custom HDP structure, see \code{\link{hdp_init}}, \code{\link{hdp_adddp}},
+#' and \code{\link{hdp_addconparam}}.
+#'
 #' @param data A \code{data.frame} or \code{matrix} of counts with one row for every sample
 #'  and one column for every data category.
-#' @param initcc Number of data clusters to start with 
+#' @param initcc Number of data clusters to start with
 #'  (every data item is randomly assigned to a cluster to start with)
-#' @param alphaa Shape hyperparameter for the gamma prior over the concentration parameter. 
+#' @param alphaa Shape hyperparameter for the gamma prior over the concentration parameter.
 #' @param alphab Rate hyperparameter for the gamma prior over the concentration parameter.
 
-#' @return A hdpState object with a basic default structure. See \code{\link{hdpState-class}}  
+#' @return A hdpState object with a basic default structure. See \code{\link{hdpState-class}}
 #' @seealso \code{\link{hdp_init}}, \code{\link{hdp_posterior}}
 #' @export
 #' @examples
@@ -25,7 +25,7 @@
 #' my_quick_hdp_chain <- hdp_posterior(my_quick_hdp, 100, 50, 10, 5)
 
 hdp_quick_init <- function(data, initcc=2, alphaa=1, alphab=1){
-  
+
   # input checks
   if (!class(data) %in% c("matrix", "data.frame")) {
     stop("data must be data.frame or matrix")
@@ -40,30 +40,30 @@ hdp_quick_init <- function(data, initcc=2, alphaa=1, alphab=1){
   if (length(alphaa) != 1 | length(alphab) != 1) {
     stop("alphaa and alphab must have length 1")
   }
-  
+
   # number of data categories, samples, DPs
   numcateg <- ncol(data)
   nsamp <- as.integer(nrow(data))
   numdp <- as.integer(nsamp + 1)
-  
+
   # hh base distribution is simple uniform Dirichlet
   hh <- rep(1, numcateg)
-  
+
   # ppindex is one 0 then all 1
   ppindex <- c(0, rep(1, nsamp))
-  
+
   # cpindex is all 1
   cpindex <- rep(1, numdp)
-  
+
   # initialise hdpState
   hdp <- hdp_init(ppindex, cpindex, hh, alphaa, alphab)
-  
+
   # add data
   hdp <- hdp_setdata(hdp, 2:numdp, data)
-  
+
   # activate all DPs and initialise clusters
   hdp <- dp_activate(hdp, 1:numdp, initcc)
-  
+
   # check validity and return
   if (!validObject(hdp)) warning("Not a valid hdpState object.")
   return(hdp)
--- R/hdp_setdata.R
+++ R/hdp_setdata.R
@@ -1,16 +1,16 @@
 #' Assign data to DP nodes in a hdpState object
-#'  
-#' Assign data to 'heldout' (state is 0) DP nodes in a hdpState object. 
+#'
+#' Assign data to 'heldout' (state is 0) DP nodes in a hdpState object.
 #' 'Heldout' DPs are not available for posterior sampling,
 #' and will need to be activated (see \code{\link{dp_activate}}). The posterior
-#' sampling process (a Gibbs sampler) is run via \code{\link{hdp_posterior}}. 
-#' 
+#' sampling process (a Gibbs sampler) is run via \code{\link{hdp_posterior}}.
+#'
 #' @param hdp A hdpState object
 #' @param dpindex Indices of the DPs to assign data to (in same order as rows of \code{data})
-#' @param data A \code{data.frame} or \code{matrix} of counts with one row for every sample 
+#' @param data A \code{data.frame} or \code{matrix} of counts with one row for every sample
 #' (same order as \code{dpindex}) and one column for every data category.
-#' @return A hdpState object updated with the new data. See \code{\link{hdpState-class}} 
-#' @seealso \code{\link{hdp_init}}, \code{\link{hdp_adddp}}, 
+#' @return A hdpState object updated with the new data. See \code{\link{hdpState-class}}
+#' @seealso \code{\link{hdp_init}}, \code{\link{hdp_adddp}},
 #'  \code{\link{dp_activate}}, \code{\link{hdp_posterior}}
 #' @export
 #' @examples
@@ -20,9 +20,9 @@
 #' my_hdp <- hdp_adddp(my_hdp, 10, c(rep(2, 5), rep(3, 5)), 3)
 #' my_hdp <- hdp_setdata(my_hdp, 4:13, example_data_hdp)
 #' dp(my_hdp)
-#' 
+
 hdp_setdata <- function(hdp, dpindex, data){
-  
+
   #input checks
   if (class(hdp) != "hdpState") stop("hdp must have class hdpState")
   if (!validObject(hdp)) stop("input hdp is not a valid hdpState object")
@@ -30,7 +30,7 @@ hdp_setdata <- function(hdp, dpindex, data){
         any(dpindex > hdp@numdp) |
         any(dpindex %% 1 != 0) |
         any(duplicated(dpindex))) {
-    stop("dpindex must be positive integers no greater than 
+    stop("dpindex must be positive integers no greater than
          numdp(hdp) with no duplicates")
   }
   if (!class(data) %in% c("matrix", "data.frame")) {
@@ -41,13 +41,13 @@ hdp_setdata <- function(hdp, dpindex, data){
   if (any(data %% 1 != 0) | any(data < 0)) {
     stop("data must contain non-negative integer values")
   }
-  
+
   # convert data to a list of data item values (not category counts)
   datass <- apply(data, 1, function(x) rep(1:ncol(data), x))
-  
-  
+
+
   HELDOUT <- 0L
-  
+
   # assign data to specified DP
   for (jj in 1:length(dpindex)){
     if (hdp@dpstate[dpindex[jj]] != HELDOUT){
@@ -56,7 +56,7 @@ hdp_setdata <- function(hdp, dpindex, data){
     hdp@dp[[dpindex[jj]]]@numdata <- length(datass[[jj]])
     hdp@dp[[dpindex[jj]]]@datass  <- datass[[jj]]
   }
-  
+
   # check validity and return
   if (!validObject(hdp)) warning("Not a valid hdpState object.")
   return(hdp)
