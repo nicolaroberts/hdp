2615c952b015691e6ef880782c5783ee69f7c9b3
Nicola Roberts <nr3@sanger.ac.uk>
2015-09-30 17:49:57 +0100

    generic and methods for extracting components

---
+++ R/aaa-generics-components.R
@@ -0,0 +1,31 @@
+#' Extract major components from the raw clusters
+#' @param x hdpSampleChain or hdpSampleMulti object
+#' @param cos.merge Merge components with cosine similarity above this threshold (default 0.90)
+#' @param prop.ex The proportion of data items explained by the components (default 0.97).
+#'  Only used for hdpSampleChain as this can be calculated automatically for hdpMultiChain objects.
+#' @return A hdpSampleChain or hdpSampleMulti object updated with component information
+#' @aliases hdp_extract_components
+#' @include hdp_extract_comp_multi.R hdp_extract_comp_single.R
+#' @export
+#' @examples
+#' hdp_extract_components(tcga_example_chain)
+#'
+setGeneric("hdp_extract_components",
+           function(x, cos.merge=0.9, prop.ex=0.97)
+             standardGeneric("hdp_extract_components"))
+
+#' @describeIn hdp_extract_components
+setMethod("hdp_extract_components",
+          signature = "hdpSampleChain",
+          definition = function(x, cos.merge, prop.ex) {
+            ans <- hdp_extract_comp_single(x, prop.ex, cos.merge)
+            return(ans)
+          })
+
+#' @describeIn hdp_extract_components
+setMethod("hdp_extract_components",
+          signature = "hdpSampleMulti",
+          definition = function(x, cos.merge) {
+            ans <- hdp_extract_comp_multi(x, cos.merge)
+            return(ans)
+          })
