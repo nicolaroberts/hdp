94a28c21c980ee6cc486dd650364cf6cbf544c02
Nicola Roberts <nr3@sanger.ac.uk>
2015-09-30 17:47:57 +0100

    data documentation

--- R/hdp.R
+++ R/hdp.R
@@ -14,7 +14,30 @@ NULL
 
 #' Posterior sampling chain from TCGA somatic mutation data
 #'
-#' Describe here
+#' HDP posterior sampling chain with data from SomaticCancerAlterations package.
+#' Categories are the 96 base substitution types defined by local trinucleotide
+#' content, and the samples include 291 glioblastoma
+#' multiforme (gbm) samples, and 293 kidney chromophobe cancer (kirc) samples.
+#' Data was derived from exome-sequencing studies.
+#' Each sample was assigned to a unique child DP node, with one parent DP node for
+#' gbm, one parent node for kirc samples, and one grandparent DP node at the top level.
+#' Chain initialised with 8 clusters, then run through 3000 burn-in iterations before
+#' collecting 50 posterior samples with 50 iterations between each.
 #'
 #' @format A hdpSampleChain object with 50 posterior samples
 "tcga_example_chain"
+
+#' Multiple posterior sampling chains with TCGA somatic mutation data
+#'
+#' Five independent HDP posterior sampling chains with data from SomaticCancerAlterations package.
+#' Categories are the 96 base substitution types defined by local trinucleotide
+#' content, and the samples include 291 glioblastoma
+#' multiforme (gbm) samples, and 293 kidney chromophobe cancer (kirc) samples.
+#' Data was derived from exome-sequencing studies.
+#' Each sample was assigned to a unique child DP node, with one parent DP node for
+#' gbm, one parent node for kirc samples, and one grandparent DP node at the top level.
+#' Each of the five chains were initialised with 8 clusters, then run through 3000 burn-in iterations before
+#' collecting 50 posterior samples with 50 iterations between each.
+#'
+#' @format A hdpSampleMulti object with 250 posterior samples, 50 from each chain
+"tcga_example_multi"
