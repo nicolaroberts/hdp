279b6630e8ea08e011d5fb801afd732d56a495fc
Nicola Roberts <nr3@sanger.ac.uk>
2016-01-22 11:35:16 +0000

    new algorithm for defining component zero

--- R/hdp_extract_comp_multi.R
+++ R/hdp_extract_comp_multi.R
@@ -10,153 +10,207 @@ hdp_extract_comp_multi <- function(chains, cos.merge=0.90, redo=TRUE){
   }
   if (class(redo) != "logical") stop("redo must be TRUE or FALSE")
 
-
-  # function to merge cols of a matrix according to new (numeric) column labels
-  merge_cols <- function(mx, labels){
-    colnames(mx) <- labels
-    num_rows <- nrow(mx)
-    # init matrix of 0s with new column names
-    ans <- matrix(0, nrow=num_rows, ncol=length(unique(labels)),
-                  dimnames=list(c(1:num_rows), sort(as.numeric(unique(labels))))
-    )
-
-    for (label in colnames(ans)){
-      ans[, label] <- rowSums(matrix(mx[, which(colnames(mx) == label)],
-                                    nrow=num_rows), na.rm=TRUE)
-    }
-    return(ans)
-  }
-
-
-  # function to merge elements of a list according to new (numeric) names
-  merge_elems <- function(lst, labels){
-    names(lst) <- labels
-    # init list with new names
-    ans <- vector("list", length(unique(labels)))
-    names(ans) <- unique(labels)
-
-    for (label in names(ans)){
-      ans[[label]] <- Reduce("+", lst[which(labels == label)])
-    }
-    return(ans[order(as.numeric(names(ans)))])
-  }
-
-  chains@comp_settings <- list(cos.merge=cos.merge)
-
   # list of hdpSampleChain objects
   chlist <- chains@chains
   nch <- length(chlist)
 
-  # extract components on each chain
+  # extract components within each chain
   for (i in 1:nch){
-
     if (!redo & length(comp_categ_counts(chlist[[i]])) > 0) next
-
-    chlist[[i]] <- hdp_extract_comp_single(chlist[[i]],
-                                           prop.ex=0.999, cos.merge=cos.merge)
+    chlist[[i]] <- hdp_extract_comp_single(chlist[[i]], cos.merge=cos.merge)
   }
-
   chains@chains <- chlist
 
+  # number of categories, DPs, samples, and data items at each DP
+  ncat <- numcateg(final_hdpState(chlist[[1]]))
+  ndp <- numdp(final_hdpState(chlist[[1]]))
+  nsamp <- sapply(chlist, function(x) hdp_settings(x)$n)
+  numdata <- sapply(dp(final_hdpState(chlist[[1]])), numdata)
 
-  # Consolidate components across chains.
-  comp_mean_dist <- lapply(chlist, function(x) t(comp_categ_distn(x)$mean))
-  nraw_comp <- sapply(comp_mean_dist, ncol) - 1
+  remove(i)
 
-  # compare against chain with minimum number of components
-  # and match to closest if cos similarity > 0.9
-  baseline <- which.min(nraw_comp)
+  # Step (1) Match up components across chains
+  # Get average comp_categ_counts matrix for each chain (average over posterior samples)
+  ccc_0 <- lapply(chlist, function(x) apply(simplify2array(comp_categ_counts(x)), 2:3, mean))
 
-  for (i in setdiff(1:nch, baseline)) {
-    new_col_names <- rep("0", nraw_comp[i])
-    for (j in 1:nraw_comp[i]) {
-      cos_sim <- apply(comp_mean_dist[[baseline]], 2, lsa::cosine,
-                       comp_mean_dist[[i]][, as.character(j)])
-      if (max(cos_sim, na.rm=TRUE) > 0.9) {
-        new_col_names[j] <- names(which.max(cos_sim))
-      }
-    }
-    new_col_names <- c("0", new_col_names)
-    colnames(comp_mean_dist[[i]]) <-  new_col_names
-  }
+  # Make each ccc matrix have the same number of columns
+  maxcomp <- max(sapply(ccc_0, ncol))
+  ccc_1 <- lapply(ccc_0, function(x){
+    ans <- cbind(x, matrix(0, nrow=ncat, ncol=(maxcomp-ncol(x)+1)))
+    return(ans[, -ncol(ans)])
+  })
 
-  comp_mapping <- lapply(comp_mean_dist, colnames)
+  remove(ccc_0)
 
-  remove(comp_mean_dist, new_col_names, baseline, cos_sim, i, j)
+  # K-centroids clustering of all components across chains
+  # with cannot-link constraints within each chain.
+  # Manhattan distance and median centroid.
+  # exclude component 0 from each chain before matching
+  ccc_1_no0 <- lapply(ccc_1, function(x) x[,-1])
+  ccc_unlist <- t(do.call(cbind, ccc_1_no0))
+  groupfactor <- rep(1:nch, each=maxcomp-1)
+  initial_clust <- rep(1:(maxcomp-1), times=nch)
 
-  # if any colname not present in all chains, set to zero.
-  comp_count <- table(unlist(sapply(comp_mapping, unique)))
-  discard <- which(comp_count < nch)
+  ccc_clust <- flexclust::kcca(ccc_unlist, k=initial_clust,
+                               group=groupfactor,
+                               family=flexclust::kccaFamily("kmedians",
+                                                    groupFun="differentClusters"))
 
-  comp_mapping <- lapply(comp_mapping, function(x) {
-    replace(x, which(x %in% names(discard)), "0")
-    })
+  # want this plot to be as simple as possible
+  # tmp <- matrix(flexclust::clusters(ccc_clust), byrow=T, ncol=maxcomp-1)
+  # matplot(tmp, type='l', lty=1, main="kmedians")
 
-  remove(comp_count, discard)
+  comp_mapping <- split(flexclust::clusters(ccc_clust), groupfactor)
+  comp_mapping <- lapply(comp_mapping, function(x) c(0, x))
 
+  remove(ccc_1, ccc_1_no0, ccc_unlist, groupfactor, initial_clust, ccc_clust)
 
-
-  # Now have comp_mapping, consolidate all stats from all chains
-
-  ncomp <- length(unique(comp_mapping[[1]]))
-
-  # comp_categ_counts (rename overall comps so sorted by mean size)
+  # Step (2) Consolidate all ccc and cdc stats from all chains
+  # Get comp_categ_counts list for each chain,
+  # make the same length, and reorder elements so they match up to comp_mapping
   ccclist <- lapply(chlist, comp_categ_counts)
-  cccmerge <- mapply(merge_elems, ccclist, comp_mapping)
-  ranks <- rank(rowMeans(matrix(sapply(cccmerge, sum), nrow=ncomp)))
-  names(ranks) <- rownames(cccmerge)
 
-  dummy <- 1
-  while (any(diff(ranks)[-1] > 0)){
-    dummy <- dummy + 1
-    which.switch <- which(diff(ranks)[-1] > 0)[1]
-    to.switch <- names(ranks)[which.switch+1:2]
-    one <- lapply(comp_mapping, function(x) which(x == to.switch[1]))
-    two <- lapply(comp_mapping, function(x) which(x == to.switch[2]))
-    comp_mapping <- mapply(function(x, y) {
-        x[y] <- to.switch[2]
-        return(x)
-      },
-      comp_mapping, one, SIMPLIFY=FALSE)
+  cccmerge <- mapply(function(x, y, z){
+    ans <- c(x, rep(list(matrix(0, nrow=y, ncol=ncat)), (maxcomp-length(x)+1)))
+    ans <- ans[-length(ans)]
+    names(ans) <- z
+    ans <- ans[order(as.numeric(names(ans)))]
+    return(ans)
+  }, ccclist, nsamp, comp_mapping, SIMPLIFY = TRUE)
 
-    comp_mapping <- mapply(function(x, y) {
-          x[y] <- to.switch[1]
-          return(x)
-        },
-        comp_mapping, two, SIMPLIFY=FALSE)
-
-    cccmerge <- mapply(merge_elems, ccclist, comp_mapping)
-    ranks <- rank(rowMeans(matrix(sapply(cccmerge, sum), nrow=ncomp)))
-    names(ranks) <- rownames(cccmerge)
-
-    if (dummy > 10) break
-
+  # rbind matrices from different chains
+  ccc_2 <- vector("list", nrow(cccmerge))
+  names(ccc_2) <- rownames(cccmerge)
+  for (i in 1:length(ccc_2)){
+    ccc_2[[i]] <- Reduce(rbind, cccmerge[i, ])
   }
 
-  ccc <- vector("list", nrow(cccmerge))
-  names(ccc) <- rownames(cccmerge)
-  for (i in 1:length(ccc)){
-    ccc[[i]] <- Reduce(rbind, cccmerge[i, ])
-  }
-  names(ccc) <- 0:(ncomp-1)
-  remove(i, ccclist, cccmerge)
-
-
-  # comp_dp_counts
+  # Get comp_dp_counts list for each chain,
+  # make number of columns the same, and reorder columns to match comp_mapping
   cdclist <- lapply(chlist, comp_dp_counts)
-  cdcmerge <- mapply(function(x, y) lapply(x, merge_cols, y), cdclist,
-                     comp_mapping, SIMPLIFY=FALSE)
-  cdc <- vector("list", length(cdcmerge[[1]]))
-  for (i in 1:length(cdc)) {
-    cdc[[i]] <- Reduce(rbind, lapply(cdcmerge, `[[`, i))
-    colnames(cdc[[i]]) <- 0:(ncomp-1)
+  cdcmerge <- mapply(function(x, y, z) {
+    x <- lapply(x, function(w){
+            ans <- cbind(w, matrix(0, nrow=y, ncol=(maxcomp-ncol(w)+1)))
+            ans <- ans[, -ncol(ans)]
+            colnames(ans) <- z
+            ans <- ans[,order(as.numeric(colnames(ans)))]
+            return(ans)
+          })
+  }, cdclist, nsamp, comp_mapping, SIMPLIFY=FALSE)
+
+  # rbind matrices from different chains
+  cdc_2 <- vector("list", length(cdcmerge[[1]]))
+  for (i in 1:length(cdc_2)) {
+    cdc_2[[i]] <- Reduce(rbind, lapply(cdcmerge, `[[`, i))
+    colnames(cdc_2[[i]]) <- 0:(maxcomp-1)
   }
-  remove(i, cdclist, cdcmerge)
+
+  remove(i, cdclist, cdcmerge, cccmerge, ccclist, chlist, comp_mapping, maxcomp)
+
+  comp_label <- names(ccc_2)
+  if(any(comp_label != colnames(cdc_2[[1]]))) stop("problem in step 2!")
+
+  # Step (3)
+  # Merge components with high cosine similarity in ccc
+  avgdistn <- sapply(ccc_2, function(x) colMeans(x)/sum(colMeans(x)))
+  clust_cos <- lsa::cosine(avgdistn)
+  clust_same <- (clust_cos > cos.merge & lower.tri(clust_cos))
+  same <- which(clust_same, arr.ind=TRUE) # merge these
+
+  # how best to track and update components??
+  # update clust_label vector to reflect the merging of columns.
+  if (length(same)>0){
+    for (i in 1:nrow(same)){
+      comp_label[same[i, 1]] <- comp_label[same[i, 2]]
+    }
+    remove(i)
+  }
+  ccc_3 <- merge_elems(ccc_2, comp_label)
+  cdc_3 <- lapply(cdc_2, merge_cols, comp_label)
+
+  remove(ccc_2, cdc_2, avgdistn, clust_cos, clust_same, same)
+
+  comp_label <- names(ccc_3)
+  if (any(comp_label != colnames(cdc_3))) stop("problem in step 3!")
 
 
+  # Step (4)
+  # Assign components with no *significantly* non-zero data categories
+  # to component '0'
+  use_comp <- c()
+  for (ii in 1:length(ccc_3)) {
+    lowerb <- apply(ccc_3[[ii]], 2, function(y) {
+      samp <- coda::as.mcmc(y)
+      if (sum(!is.nan(samp)) ==  1) {
+        NaN
+      } else {
+        round(coda::HPDinterval(samp)[1], 3)
+      }
+    })
+    if(any(lowerb>0)) use_comp <- c(use_comp, names(ccc_3)[ii])
+  }
+  # update clust_label vector
+  comp_label[which(!comp_label %in% use_comp)] <- 0
+  ccc_4 <- merge_elems(ccc_3, comp_label)
+  cdc_4 <- lapply(cdc_3, merge_cols, comp_label)
+
+  remove(ccc_3, cdc_3, ii, lowerb, use_comp)
+
+  comp_label <- names(ccc_4)
+  if (any(comp_label != colnames(cdc_4))) stop("problem in step 4!")
+
+  # Step (5)
+  # Assign components with no *significantly* non-zero sample exposure
+  # to component '0' (disregarding DP nodes with no data items (parent nodes))
+  use_comp <- c()
+  disregard <- which(numdata==0)
+  for (ii in 1:ncol(cdc_4[[1]])) {
+    compii <- t(sapply(cdc_4, function(x) x[,ii]))
+    lowerb <- apply(compii[-disregard,], 1, function(y) {
+      samp <- coda::as.mcmc(y)
+      if (sum(!is.nan(samp)) ==  1) {
+        NaN
+      } else {
+        round(coda::HPDinterval(samp)[1], 3)
+      }
+    })
+    if(any(lowerb>0)) use_comp <- c(use_comp, colnames(cdc_4[[1]])[ii])
+  }
+  # update comp_label vector
+  comp_label[which(!comp_label %in% use_comp)] <- 0
+  ccc_5 <- merge_elems(ccc_4, comp_label)
+  cdc_5 <- lapply(cdc_4, merge_cols, comp_label)
+
+  remove(compii, ccc_4, cdc_4, ii, lowerb, use_comp)
+
+  comp_label <- names(ccc_5)
+  if (any(comp_label != colnames(cdc_5))) stop("problem in step 5!")
+
+  # Step (6)
+  # Rename overall component, order by number of data items (on average)
+  # 0th component still goes first
+  avg_ndi <- sapply(ccc_5, function(x) mean(rowSums(x)))
+  comporder <- c(1, setdiff(order(avg_ndi, decreasing=T), 1))
+
+  ccc_ans <- ccc_5[comporder]
+  names(ccc_ans) <- 0:(length(ccc_ans)-1)
+
+  cdc_ans <- lapply(cdc_5, function(x) {
+    x <- x[, comporder]
+    colnames(x) <- 0:(ncol(x)-1)
+    return(x)
+  })
+
+  # number of components
+  ncomp <- length(comporder)
+
+  remove(ccc_5, cdc_5, avg_ndi, comporder)
+
+
+  # Step (7)
   # Calculate mean and 95% credibility interval for each component's
   # categorical data distribution
-  ccc_norm <- lapply(ccc, function(x) x/rowSums(x, na.rm=TRUE))
+  ccc_norm <- lapply(ccc_ans, function(x) x/rowSums(x, na.rm=TRUE))
 
   ccc_mean <- t(sapply(ccc_norm, colMeans, na.rm=TRUE))
   rownames(ccc_mean) <- 0:(ncomp-1)
@@ -175,7 +229,7 @@ hdp_extract_comp_multi <- function(chains, cos.merge=0.90, redo=TRUE){
 
   # Calculate mean and 95% credibility interval for each DP's
   # distribution over components (counts)
-  cdc_norm <- lapply(cdc, function(x) x/rowSums(x, na.rm=TRUE))
+  cdc_norm <- lapply(cdc_ans, function(x) x/rowSums(x, na.rm=TRUE))
 
   cdc_mean <- t(sapply(cdc_norm, colMeans, na.rm=TRUE))
 
@@ -190,14 +244,17 @@ hdp_extract_comp_multi <- function(chains, cos.merge=0.90, redo=TRUE){
     })
   })
 
+  # add extracted components into hdpSampleMulti slots
+  chains@numcomp <- as.integer(ncomp - 1)
 
   # proportion of data explained by extracted components?
-  avcount <- colMeans(sapply(ccc, rowSums, na.rm=TRUE), na.rm=TRUE)
+  avcount <- colMeans(sapply(ccc_ans, rowSums, na.rm=TRUE), na.rm=TRUE)
   chains@prop.ex <- round(1-avcount[1]/sum(avcount), 3)
 
-  # add extracted components into hdpSampleMulti slots
-  chains@comp_categ_counts <- ccc
-  chains@comp_dp_counts <- lapply(cdc, as, "dgCMatrix")
+  chains@comp_cos_merge <- cos.merge
+
+  chains@comp_categ_counts <- ccc_ans
+  chains@comp_dp_counts <- lapply(cdc_ans, as, "dgCMatrix")
   chains@comp_categ_distn <- list(mean=ccc_mean,
                                  cred.int=ccc_credint)
   chains@comp_dp_distn <- list(mean=cdc_mean,
--- R/hdp_extract_comp_single.R
+++ R/hdp_extract_comp_single.R
@@ -1,40 +1,26 @@
-hdp_extract_comp_single <- function(chain, prop.ex=0.97, cos.merge=0.90){
-
-  set.seed(hdp_seed(chain), kind="Mersenne-Twister", normal.kind="Inversion")
+ hdp_extract_comp_single <- function(chain, cos.merge=0.90){
 
   # input checks
   if (class(chain) != "hdpSampleChain") {
     stop("chain must have class hdpSampleChain")
   }
   if (!validObject(chain)) stop("chain not a valid hdpSampleChain object")
-  if (class(prop.ex) != "numeric" | prop.ex >=1 | prop.ex <= 0) {
-    stop("prop.ex must be between 0 and 1")
-  }
   if (class(cos.merge) != "numeric" | cos.merge >=1 | cos.merge <= 0) {
     stop("cos.merge must be between 0 and 1")
   }
 
-  # number of categories, DPs, and samples
+  set.seed(sampling_seed(chain), kind="Mersenne-Twister", normal.kind="Inversion")
+
+  # number of categories, DPs, samples, data items at each DP, and frozen priors
   ncat <- numcateg(final_hdpState(chain))
   ndp <- numdp(final_hdpState(chain))
   nsamp <- hdp_settings(chain)$n
-
-  # function to merge cols of a matrix according to new (numeric) column labels
-  merge_cols <- function(mx, labels){
-    colnames(mx) <- labels
-    num_rows <- nrow(mx)
-    # init matrix of 0s with new column names
-    ans <- matrix(0, nrow=num_rows, ncol=length(unique(labels)),
-                  dimnames=list(c(1:num_rows), sort(as.numeric(unique(labels))))
-                  )
-
-    for (label in colnames(ans)){
-      ans[, label] <- rowSums(matrix(mx[, which(colnames(mx) == label)],
-                                    nrow=num_rows))
-    }
-    return(ans)
+  numdata <- sapply(dp(final_hdpState(chain)), numdata)
+  pseudo <- pseudoDP(final_hdpState(chain))
+  is_prior <- length(pseudo) > 0
+  if (is_prior) {
+    priorcc <- 1:length(pseudo)
   }
-
 
   # Step (1)
   # Make each ccc (clust_categ_counts) and
@@ -57,14 +43,20 @@ hdp_extract_comp_single <- function(chain, prop.ex=0.97, cos.merge=0.90){
   # Match up raw clusters (matrix columns) across posterior samples (columns not
   # guaranteed to keep same component through all samples)
   # K-centroids clustering of all raw clusters with cannot-link constraints
-  # within each posterior sample
+  # within each posterior sample, Manhattan distance and median centroid
   ccc_unlist <- t(do.call(cbind, ccc_1))
   groupfactor <- rep(1:nsamp, each=maxclust)
+  initial_clust <- rep(1:maxclust, times=nsamp)
 
-  ccc_clust <- flexclust::kcca(ccc_unlist, maxclust,
+  ccc_clust <- flexclust::kcca(ccc_unlist, k=initial_clust,
                            group=groupfactor,
-                           family=flexclust::kccaFamily("kmeans",
+                           family=flexclust::kccaFamily("kmedians",
                                              groupFun="differentClusters"))
+
+  # want this plot to be as simple as possible
+  # tmp <- matrix(flexclust::clusters(ccc_clust), byrow=T, ncol=maxclust)
+  # matplot(tmp, type='l', lty=1, main="kmedians")
+
   ccc_label <- split(flexclust::clusters(ccc_clust), groupfactor)
 
   ccc_2 <- mapply(function(ccc, label) {
@@ -79,7 +71,7 @@ hdp_extract_comp_single <- function(chain, prop.ex=0.97, cos.merge=0.90){
     },
                   cdc_1, ccc_label, SIMPLIFY=FALSE)
 
-  remove(ccc_1, cdc_1, ccc_unlist, groupfactor, ccc_clust, ccc_label)
+  remove(ccc_1, cdc_1, ccc_unlist, groupfactor, initial_clust, ccc_clust, ccc_label)
 
   # Step (3)
   # Merge the ccc columns with high cosine similarity.
@@ -96,47 +88,80 @@ hdp_extract_comp_single <- function(chain, prop.ex=0.97, cos.merge=0.90){
     for (i in 1:nrow(same)){
       clust_label[same[i, 1]] <- clust_label[same[i, 2]]
     }
+    remove(i)
   }
   ccc_3 <- lapply(ccc_2, merge_cols, clust_label)
+  cdc_3 <- lapply(cdc_2, merge_cols, clust_label)
+  clust_label <- colnames(ccc_3[[1]])
+  if (any(clust_label != colnames(cdc_3))) stop("problem in step 3!")
 
-  remove(avgdistn, i, distns, clust_cos, clust_same, same)
+  remove(avgdistn, distns, clust_cos, clust_same, same, ccc_2, cdc_2)
 
 
   # Step (4)
-  # Keep the largest components that explain at LEAST prop.ex
-  # proportion of the data
-  # and assign the leftover clusters to component '0'
-  avg_prop_data <- rowMeans(sapply(ccc_3, colSums))/sum(ccc_3[[1]])
-  cl_ordered <- names(avg_prop_data)[order(avg_prop_data, decreasing=T)]
-  cl_below_prop <- which(cumsum(avg_prop_data[cl_ordered]) < prop.ex)
-  if (length(cl_below_prop) == 0) {
-    use_clust <- cl_ordered
-  } else {
-    use_clust <- cl_ordered[1:(max(cl_below_prop)+1)]
+  # Assign components with no *significantly* non-zero data categories
+  # to component '0'
+  use_clust <- c()
+  for (ii in 1:ncol(ccc_3[[1]])) {
+    compii <- sapply(ccc_3, function(x) x[,ii])
+    lowerb <- apply(compii, 1, function(y) {
+                samp <- coda::as.mcmc(y)
+                if (sum(!is.nan(samp)) ==  1) {
+                  NaN
+                } else {
+                  round(coda::HPDinterval(samp)[1], 3)
+                }
+              })
+    if(any(lowerb>0)) use_clust <- c(use_clust, colnames(ccc_3[[1]])[ii])
   }
-  # update clust_label vector to reflect the merging of the small
-  # clusters into the 0-th component
+  # update clust_label vector
   clust_label[which(!clust_label %in% use_clust)] <- 0
-  ccc_4 <- lapply(ccc_2, merge_cols, clust_label)
-  cdc_4 <- lapply(cdc_2, merge_cols, clust_label)
+  ccc_4 <- lapply(ccc_3, merge_cols, clust_label)
+  cdc_4 <- lapply(cdc_3, merge_cols, clust_label)
+  clust_label <- colnames(ccc_4[[1]])
+  if (any(clust_label != colnames(cdc_4))) stop("problem in step 4!")
 
-  remove(ccc_2, ccc_3, cdc_2, avg_prop_data,
-         cl_below_prop, cl_ordered, use_clust, clust_label)
-
+  remove(compii, ccc_3, cdc_3, ii, lowerb, use_clust)
 
   # Step (5)
+  # Assign components with no *significantly* non-zero sample exposure
+  # to component '0' (disregarding DP nodes with no data items (parent nodes))
+  use_clust <- c()
+  disregard <- which(numdata==0)
+  for (ii in 1:ncol(cdc_4[[1]])) {
+    compii <- sapply(cdc_4, function(x) x[,ii])
+    lowerb <- apply(compii[-disregard,], 1, function(y) {
+      samp <- coda::as.mcmc(y)
+      if (sum(!is.nan(samp)) ==  1) {
+        NaN
+      } else {
+        round(coda::HPDinterval(samp)[1], 3)
+      }
+    })
+    if(any(lowerb>0)) use_clust <- c(use_clust, colnames(cdc_4[[1]])[ii])
+  }
+  # update clust_label vector
+  clust_label[which(!clust_label %in% use_clust)] <- 0
+  ccc_5 <- lapply(ccc_4, merge_cols, clust_label)
+  cdc_5 <- lapply(cdc_4, merge_cols, clust_label)
+  clust_label <- colnames(ccc_5[[1]])
+  if (any(clust_label != colnames(cdc_5))) stop("problem in step 5!")
+
+  remove(compii, ccc_4, cdc_4, ii, lowerb, use_clust)
+
+  # Step (6)
   # Rename overall component, order by number of data items (on average)
   # 0th component still goes first
-  avg_ndi <- rowMeans(sapply(ccc_4, colSums))
+  avg_ndi <- rowMeans(sapply(ccc_5, colSums))
   colorder <- c(1, setdiff(order(avg_ndi, decreasing=T), 1))
 
-  ccc_5 <- lapply(ccc_4, function(x) {
+  ccc_6 <- lapply(ccc_5, function(x) {
     x <- x[, colorder]
     colnames(x) <- 0:(ncol(x)-1)
     return(x)
   })
 
-  cdc_5 <- lapply(cdc_4, function(x) {
+  cdc_6 <- lapply(cdc_5, function(x) {
     x <- x[, colorder]
     colnames(x) <- 0:(ncol(x)-1)
     return(x)
@@ -145,26 +170,26 @@ hdp_extract_comp_single <- function(chain, prop.ex=0.97, cos.merge=0.90){
   # number of components
   ncomp <- length(colorder)
 
-  remove(ccc_4, cdc_4, avg_ndi, colorder)
+  remove(ccc_5, cdc_5, avg_ndi, colorder)
 
 
-  # Step (6)
+  # Step (7)
   # Convert ccc into list of length ncomp, with matrices nsamp*ncat
   ccc_ans <- rep(list(matrix(0, nrow=nsamp, ncol=ncat)), ncomp)
   for (i in 1:ncomp){
-    ccc_ans[[i]] <- t(sapply(ccc_5, function(x) x[, i]))
+    ccc_ans[[i]] <- t(sapply(ccc_6, function(x) x[, i]))
   }
 
   # Convert cdc into list of length ndp, with matrices nsamp*ncomp
   cdc_ans <- rep(list(matrix(0, nrow=nsamp, ncol=ncomp)), ndp)
   for (i in 1:ndp){
-    cdc_ans[[i]] <- t(sapply(cdc_5, function(x) x[i, ]))
+    cdc_ans[[i]] <- t(sapply(cdc_6, function(x) x[i, ]))
   }
 
-  remove(ccc_5, cdc_5)
+  remove(ccc_6, cdc_6)
 
 
-  # Step (7)
+  # Step (8)
   # Calculate mean and 95% credibility interval for each component's
   # categorical data distribution
   ccc_norm <- lapply(ccc_ans, function(x) x/rowSums(x, na.rm=TRUE))
@@ -203,8 +228,13 @@ hdp_extract_comp_single <- function(chain, prop.ex=0.97, cos.merge=0.90){
   })
 
   # add extracted components into chain hdpSampleChain slots
-  chain@comp_settings <- list(prop.ex=prop.ex,
-                              cos.merge=cos.merge)
+  chain@numcomp <- as.integer(ncomp - 1)
+
+  # proportion of data explained by extracted components?
+  avcount <- colMeans(sapply(ccc_ans, rowSums, na.rm=TRUE), na.rm=TRUE)
+  chain@prop.ex <- round(1-avcount[1]/sum(avcount), 3)
+
+  chain@comp_cos_merge <- cos.merge
 
   chain@comp_categ_counts <- ccc_ans
   chain@comp_dp_counts <- lapply(cdc_ans, as, "dgCMatrix")
